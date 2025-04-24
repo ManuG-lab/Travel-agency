ad8744de0e98981506a6f5d419f294ac5a6e4c55
Emmanuel Gitau <gmannu2055@gmail.com>
2025-04-24 16:26:51 +0300

    cleared edit and add

--- src/Admin/AdminDashboard.jsx
+++ src/Admin/AdminDashboard.jsx
@@ -1,39 +1,9 @@
-import React, { useEffect, useState } from "react";
-import EditDestinationForm from "./EditDestination";
-import AddDestinationForm from "./AddDestination";
 import AdminNavbar from "./AdminNavBar";
 
-function AdminDashboard() {
-  const [destinations, setDestinations] = useState([]);
-  const [editing, setEditing] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/destinations")
-      .then((res) => res.json())
-      .then((data) => setDestinations(data));
-  }, []);
-
-  const handleDelete = (id) => {
-    fetch(`http://localhost:3000/destinations/${id}`, { method: "DELETE" })
-      .then((res) => res.json())
-      .then(() => {
-        setDestinations((prev) => prev.filter((d) => d.id !== id));
-      });
-  };
-
-  const handleUpdate = (updated) => {
-    setDestinations((prev) =>
-      prev.map((d) => (d.id === updated.id ? updated : d))
-    );
-    setEditing(null);
-  };
-
- 
-  return (
-    <div>
-      <AdminNavbar />
-    </div>
-  );
+function AdminDashboard(){
+  return(
+    <AdminNavbar/>
+  )
 }
 
-export default AdminDashboard;
+export default AdminDashboard
