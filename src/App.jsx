55fdd23649d7aa9d60a2c10f405cbfb9895bff35
Emmanuel Gitau <gmannu2055@gmail.com>
2025-04-24 13:29:37 +0300

    updated navbar and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
 import { BrowserRouter, Routes, Route  } from "react-router-dom";
+import { useState } from "react";
 import Landing from "./Pages/Landing";
 import Navbar from "./Components/NavBar";
-import Home from "./Pages/Home";
 import Bookings from "./Pages/Bookings";
 import Contact from "./Pages/Contact";
 import About from "./Pages/About";
 import Footer from "./Components/Footer";
 import Destinations from "./Pages/Destinations";
+import AdminRoute from "./Components/AdminRoute";
+import AdminLogin from "./Pages/AdminLogin";
+import AdminDashboard from "./Pages/AdminDashboard";
 
 function App(){
+const [isAdmin, setIsAdmin] = useState(false);
   return(
     <div>
     <BrowserRouter>
     <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Home />} />
+       
         <Route path="/destinations" element={<Destinations />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/my-bookings" element={<Bookings />} />
+        <Route path="/adminLogin" element={<AdminLogin setIsAdmin={setIsAdmin} />} />
+        <Route path="/admin" element={ <AdminRoute isAdmin={isAdmin}><AdminDashboard /></AdminRoute>} />
       </Routes>
       <Footer />
     </BrowserRouter>
